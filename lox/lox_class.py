8828f8d7a19768c0cde01b00d23c2089ac742711
Paul Winkler <slinkp@gmail.com>
2023-09-14 23:07:43 -0400

    Classes, 12.3: We can now instantiate them by calling the class

--- lox/lox_class.py
+++ lox/lox_class.py
@@ -1,6 +1,22 @@
-class LoxClass:
+from .lox_callable import LoxCallable
+
+class LoxClass(LoxCallable):
     def __init__(self, name: str):
         self.name = name
 
     def __str__(self):
         return self.name
+
+    def arity(self) -> int:
+        return 0
+
+    def call(self, interpreter, arguments: list[object]):
+        return LoxInstance(self)
+
+
+class LoxInstance:
+    def __init__(self, klass: LoxClass):
+        self.klass = klass
+
+    def __str__(self):
+        return self.klass.name + " instance"
